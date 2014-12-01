6076b465b8274bfde96544833f21cbdf9d7dcfba
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-01 11:37:37 -0800

    Network: dropRate.

--- src/java/src/sawProtocolSimulator/Network.java
+++ src/java/src/sawProtocolSimulator/Network.java
@@ -2,5 +2,12 @@ package sawProtocolSimulator;
 
 public class Network
 {
-
+    /**
+     * The drop rate.
+     * 
+     * A random number will be generated for each packet received and if that random number is below
+     * or equal to this drop rate, that packet will be dropped.
+     */
+    private int dropRate;
+    
 }
