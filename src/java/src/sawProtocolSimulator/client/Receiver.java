f6e19ebb1c7efea6a557dc70999d187c0581b52e
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-02 11:33:04 -0800

    Receiver: current sequence number.

--- src/java/src/sawProtocolSimulator/client/Receiver.java
+++ src/java/src/sawProtocolSimulator/client/Receiver.java
@@ -1,7 +1,16 @@
 package sawProtocolSimulator.client;
 
+import sawProtocolSimulator.models.Packet;
+import sawProtocolSimulator.utilities.PacketUtilities;
+
 public class Receiver extends Client
 {
+    /**
+     * Current sequence number.
+     * 
+     * Initialized to 0 in the constructor.
+     */
+    private int currentSequenceNumber;
 
     /**
      * Create a client whose sole purpose is to receive from the sender (transmitter).
@@ -11,20 +20,32 @@ public class Receiver extends Client
     public Receiver(ClientMode clientMode)
     {
         super(clientMode);
+        this.currentSequenceNumber = 0;
     }
 
     @Override
     public void run()
     {
-        //listen for SOT
-        
-        //scan each packet
-        
-        //send an ack for each packet
-        
-        //if a packet arrives again, send ack again
-        
-        //listen for EOT
+        // listen for SOT
+
+        // scan each packet
+
+        // send an ack for each packet
+
+        // if a packet arrives again, send ack again
+
+        // listen for EOT
+    }
+
+    @Override
+    protected Packet makePacket(int packetType)
+    {
+        return PacketUtilities.makePacket(this.configuration.getTransmitterAddress()
+                .getHostAddress(), this.configuration.getTransmitterPort(), this.configuration
+                .getReceiverAddress().getHostAddress(), this.configuration.getReceiverPort(),
+                packetType, this.currentSequenceNumber, this.currentSequenceNumber, this.configuration
+                        .getWindowSize());
+
     }
 
 }
