a1edda43aec2a99a49ba356e25b0fd5d94d12a8b
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-02 15:59:24 -0800

    Log to file.

--- src/java/src/sawProtocolSimulator/utilities/Log.java
+++ src/java/src/sawProtocolSimulator/utilities/Log.java
@@ -1,8 +1,16 @@
 package sawProtocolSimulator.utilities;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Log
 {
-    
+
     /**
      * Simply logs to the command line and a file.
      * 
@@ -10,10 +18,34 @@ public class Log
      */
     public static void d(String log)
     {
-        //print to screen for now
-        System.out.println(log);
+        log = Log.getCurrentTime() + ":--  " + log;
         
-        //log to file later
+        // print to screen for now
+        System.out.println(log);
+
+        // log to file
+        try
+        {
+            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("logs.log")));
+            printWriter.println(log);
+        }
+        catch (IOException e)
+        {
+            //ignore
+        }
     }
-    
+
+    /**
+     * Returns the current time and date.
+     * 
+     * @return current time and date
+     */
+    public static String getCurrentTime()
+    {
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        Calendar calobj = Calendar.getInstance();
+
+        return (df.format(calobj.getTime()));
+    }
+
 }
