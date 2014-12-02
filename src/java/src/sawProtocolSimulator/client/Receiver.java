b88eeb35e59a1c2b0a5c72ac6c1f42b80f8bd874
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-02 11:58:50 -0800

    Receiver: Wait for SOT packet.

--- src/java/src/sawProtocolSimulator/client/Receiver.java
+++ src/java/src/sawProtocolSimulator/client/Receiver.java
@@ -1,6 +1,11 @@
 package sawProtocolSimulator.client;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 import sawProtocolSimulator.models.Packet;
+import sawProtocolSimulator.network.UDPNetwork;
 import sawProtocolSimulator.utilities.PacketUtilities;
 
 public class Receiver extends Client
@@ -11,6 +16,11 @@ public class Receiver extends Client
      * Initialized to 0 in the constructor.
      */
     private int currentSequenceNumber;
+    
+    /**
+     * The UDP socket, the receiver is listening on.
+     */
+    private DatagramSocket listen;
 
     /**
      * Create a client whose sole purpose is to receive from the sender (transmitter).
@@ -21,12 +31,23 @@ public class Receiver extends Client
     {
         super(clientMode);
         this.currentSequenceNumber = 0;
+        
+        try
+        {
+            this.listen = UDPNetwork.createServer(this.configuration.getReceiverPort());
+        }
+        catch (SocketException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run()
     {
         // listen for SOT
+        this.waitForOtherSideToTakeControl();
 
         // scan each packet
 
@@ -35,6 +56,32 @@ public class Receiver extends Client
         // if a packet arrives again, send ack again
 
         // listen for EOT
+    }
+    
+    /**
+     * Waits for Sender to send a SOT.
+     */
+    private void waitForOtherSideToTakeControl()
+    {
+        try
+        {
+            Packet sotPacket = UDPNetwork.getPacket(this.listen);
+            
+            if(sotPacket.getPacketType() == PacketUtilities.PACKET_START_OF_TRANSMISSION)
+            {
+                //TODO: print SOT packet.
+            }
+        }
+        catch (ClassNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @Override
