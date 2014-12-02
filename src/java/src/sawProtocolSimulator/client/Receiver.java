756141c963d6b6801961331294cc51f9c6a2f691
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-02 12:39:10 -0800

    Looks like Client is finished.

--- src/java/src/sawProtocolSimulator/client/Receiver.java
+++ src/java/src/sawProtocolSimulator/client/Receiver.java
@@ -3,6 +3,7 @@ package sawProtocolSimulator.client;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.ArrayList;
 
 import sawProtocolSimulator.models.Packet;
 import sawProtocolSimulator.network.UDPNetwork;
@@ -15,12 +16,17 @@ public class Receiver extends Client
      * 
      * Initialized to 0 in the constructor.
      */
-    private int            currentSequenceNumber;
+    private int               currentSequenceNumber;
+
+    /**
+     * Total number of ack'ed packets.
+     */
+    private ArrayList<Packet> ackedPackets;
 
     /**
      * The UDP socket, the receiver is listening on.
      */
-    private DatagramSocket listen;
+    private DatagramSocket    listen;
 
     /**
      * Create a client whose sole purpose is to receive from the sender (transmitter).
@@ -49,13 +55,100 @@ public class Receiver extends Client
         // listen for SOT
         this.waitForOtherSideToTakeControl();
 
-        // scan each packet
+        /**
+         * Receive until this is true.
+         */
+        boolean keepReceiving = true;
 
-        // send an ack for each packet
+        int totalPackets = 0;
+        int totalDuplicateAcks = 0;
 
-        // if a packet arrives again, send ack again
+        while (keepReceiving)
+        {
+            try
+            {
+                // scan each packet
+                Packet packet = UDPNetwork.getPacket(listen);
 
-        // listen for EOT
+                switch (packet.getPacketType())
+                {
+                    case (PacketUtilities.PACKET_END_OF_TRANSMISSION):
+                        // listen for EOT
+                        // TODO: print EOT .. session stats and end.
+
+                        keepReceiving = false;
+
+                        break;
+
+                    case (PacketUtilities.PACKET_DATA):
+
+                        // if packet hasn't been ACK'ed before.
+                        if (!this.findIfPacketAckedBefore(packet.getSeqNum()))
+                        {
+                            totalPackets++;
+                        }
+                        else
+                        {
+                            // ACKing again - earlier ACK probably got lost
+                            totalDuplicateAcks++;
+                        }
+
+                        // update current sequence number
+                        this.currentSequenceNumber = packet.getSeqNum();
+
+                        // TODO: print data
+
+                        // craft and send ACK packet
+                        this.sendPacket(this.makePacket(PacketUtilities.PACKET_ACK));
+
+                        // add to list of ack'ed packets
+                        this.ackedPackets.add(packet);
+
+                        // TODO: print ACK packet
+
+                        break;
+
+                    default:
+                        // TODO: packet type not valid - do something
+
+                        break;
+                }
+
+                // send an ack for each packet
+
+                // if a packet arrives again, send ack again
+
+            }
+            catch (ClassNotFoundException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Find if the current packet has ever been ack'ed before.
+     * 
+     * @param seqNum sequence number of the current packet
+     * @return true if it has been ack'ed before, false if not.
+     */
+    private boolean findIfPacketAckedBefore(int seqNum)
+    {
+        for (int i = 0; i < this.ackedPackets.size(); i++)
+        {
+            if (this.ackedPackets.get(i).getSeqNum() == seqNum)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
