0024a576fa3ff4ad0013b145de2962b07cadd348
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-12-02 13:52:33 -0800

    Log file.

---
+++ src/java/src/sawProtocolSimulator/utilities/Log.java
@@ -0,0 +1,19 @@
+package sawProtocolSimulator.utilities;
+
+public class Log
+{
+    
+    /**
+     * Simply logs to the command line and a file.
+     * 
+     * @param log the message to log.
+     */
+    public static void d(String log)
+    {
+        //print to screen for now
+        System.out.println(log);
+        
+        //log to file later
+    }
+    
+}
